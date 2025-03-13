ac62ecc69bb198142f51b16a100529e1a65f2ba8
avinashbendale <54184303+avinashbendale@users.noreply.github.com>
2025-03-13 10:14:20 +0530

    Second Commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form';
-import { Container, Row, Col } from 'react-bootstrap'
+import {Row, Col } from 'react-bootstrap'
 import "./Contact.css";
 
 const Contact = () => {
